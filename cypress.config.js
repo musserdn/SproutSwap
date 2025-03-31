4da64288efdc600df35e3f88672a4fd6138fcc8b
musserdn <musserdn@gmail.com>
2025-03-30 20:35:49 -0500

    updating scripts

--- cypress.config.js
+++ cypress.config.js
@@ -8,13 +8,6 @@ export default defineConfig({
     },
   },
 
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
