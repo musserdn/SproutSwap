479345d6fd54fdeaf0ab17d5518a5f883b088bf5
ngin2894 <nataliegindraux@gmail.com>
2025-04-01 18:36:55 -0500

    Added base HTML for the SearchFriends.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,32 +2,23 @@ const Home = () => {
     return (
         <div className="home">
             <section>
-                <h1>Welcome to the Home Page</h1>
-                <p>This is a placeholder text for the home page.</p>
+                <h1>My Garden</h1>
             </section>
 
             <section>
-                <h2>About This Page</h2>
-                <p>
-                    This page is a placeholder for your content. You can update this with
-                    information about your website or project.
-                </p>
+                <h2>My Flowers</h2>
             </section>
 
             <section>
-                <h2>Placeholder Section</h2>
-                <p>
-                    More filler text. This section can be used to highlight features, provide
-                    information, or add whatever content you like.
-                </p>
+                <h2>My Vegetables</h2>
             </section>
 
             <section>
-                <h2>Another Placeholder</h2>
-                <p>
-                    Keep adding as many sections as you need! This one can be for a list of
-                    things or anything you want.
-                </p>
+                <h2>Indoor Plants</h2>
+            </section>
+
+            <section>
+                <h2>Outdoor Plants</h2>
             </section>
         </div>
     );
