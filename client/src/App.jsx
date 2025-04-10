78b68f9e41bcc1a437d0dfa78a22958f17fec67a
ikealmighty <isaacyates7@gmail.com>
2025-04-09 21:18:57 -0500

    added ApolloProvider so that we can make graphql calls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,39 @@
-import { Outlet, useLocation } from 'react-router-dom'; // Import useLocation
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Outlet, useLocation } from "react-router-dom"; // Import useLocation
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { getAuthHeaders } from "./utils/auth";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  const authHeaders = getAuthHeaders();
+  return {
+    headers: {
+      ...headers,
+      ...authHeaders,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const location = useLocation(); // Get the current route
-  const hideNavbarRoutes = ['/login', '/create-account']; // Define routes where the navbar should be hidden
+  const hideNavbarRoutes = ["/login", "/create-account"]; // Define routes where the navbar should be hidden
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <Header />
       {/* Only show Navbar if the current path is NOT in the hideNavbarRoutes array */}
       {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
@@ -16,7 +41,7 @@ function App() {
         <Outlet />
       </main>
       <Footer />
-    </>
+    </ApolloProvider>
   );
 }
 
