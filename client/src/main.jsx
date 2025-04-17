421334b71b66702046893dcdafb6f668ac046450
ikealmighty <isaacyates7@gmail.com>
2025-04-16 21:47:54 -0500

    protected routes from users that are not logged in

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,75 +1,108 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-import App from './App.jsx';
-import ErrorPage from './pages/ErrorPage.jsx';
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Logout from './pages/Logout.jsx';
-import BlogCilantro from './pages/BlogCilantro.jsx';
-import CreateAccount from './pages/CreateAccount.jsx';
-import FriendsResults from './pages/FriendsResults.jsx';
-import SearchPlants from './pages/SearchPlants.jsx';
-import PlantDetails from './pages/PlantDetails.jsx';
-import SearchBlogs from './pages/SearchBlogs.jsx';
-import SearchFriends from './pages/SearchFriends.jsx';
-import UserProfile from './pages/UserProfile.jsx';
+import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Logout from "./pages/Logout.jsx";
+import BlogCilantro from "./pages/BlogCilantro.jsx";
+import CreateAccount from "./pages/CreateAccount.jsx";
+import FriendsResults from "./pages/FriendsResults.jsx";
+import SearchPlants from "./pages/SearchPlants.jsx";
+import PlantDetails from "./pages/PlantDetails.jsx";
+import SearchBlogs from "./pages/SearchBlogs.jsx";
+import SearchFriends from "./pages/SearchFriends.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <Home />,
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'login', 
+        path: "login",
         element: <Login />,
       },
       {
-        path: 'BlogCilantro',
-        element: <BlogCilantro />,
+        path: "BlogCilantro",
+        element: (
+          <ProtectedRoute>
+            <BlogCilantro />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'CreateAccount',
+        path: "CreateAccount",
         element: <CreateAccount />,
       },
       {
-        path: 'FriendsResults',
-        element: <FriendsResults />,
+        path: "FriendsResults",
+        element: (
+          <ProtectedRoute>
+            <FriendsResults />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'SearchPlants',
-        element: <SearchPlants />,
+        path: "SearchPlants",
+        element: (
+          <ProtectedRoute>
+            <SearchPlants />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'PlantDetails/:plantId',
-        element: <PlantDetails/>,
+        path: "PlantDetails/:plantId",
+        element: (
+          <ProtectedRoute>
+            <PlantDetails />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'searchblogs',
-        element: <SearchBlogs />,
+        path: "searchblogs",
+        element: (
+          <ProtectedRoute>
+            <SearchBlogs />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'searchfriends',
-        element: <SearchFriends />,
+        path: "searchfriends",
+        element: (
+          <ProtectedRoute>
+            <SearchFriends />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'UserProfile',
-        element: <UserProfile />,
+        path: "UserProfile",
+        element: (
+          <ProtectedRoute>
+            <UserProfile />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: 'logout',
-        element: <Logout />
-      }
+        path: "logout",
+        element: <Logout />,
+      },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
