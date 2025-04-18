2055a6803cbbbd7848e7efc160b2c566992dafeb
musserdn <musserdn@gmail.com>
2025-04-17 20:49:44 -0500

    updated global and local styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import { setContext } from "@apollo/client/link/context";
 import { css } from "@emotion/react";
 import React, { useState } from "react";
 import PlantContext from "./context/PlantContext";
+import './App.css';
 
 const httpLink = createHttpLink({
   uri: "/graphql",
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,22 +7,14 @@ const Home = () => {
   return (
     <div className="home-container">
       <h1>My Garden</h1>
-      <button 
-        onClick={() => navigate('/SearchPlants')} 
-        style={{
-          backgroundColor: '#84b254',
-          color: 'white',
-          border: 'none',
-          padding: '10px 20px',
-          borderRadius: '5px',
-          fontSize: '16px',
-          marginBottom: '20px'
-        }}
+      <button
+        onClick={() => navigate('/SearchPlants')}
+        className="btn btn-primary"
       >
         Add plants to garden
       </button>
 
-      {/* ...rest of your page... */}
+
     </div>
   );
 };
