245d0eecfb80cfdcb4feb144009eb038c024ce27
musserdn <musserdn@gmail.com>
2025-04-19 10:33:16 -0500

    updated home and header css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,11 @@
 const Header = () => {
     return (
-        <header className="header">
-            <div className="logo-container">
-                <img src="/src/assets/SproutSwapLogoNoWords.jpg" alt="SproutSwap Logo" className="logo" />
-                <h1>SproutSwap</h1>
-            </div>
-            <h3 className="tagline">Welcome to your garden!</h3>
-        </header>
+        <div>
+            <img src="/src/assets/SproutSwapLogoNoWords.jpg" alt="SproutSwap Logo" className="logo" />
+            <h1>SproutSwap</h1>
+            <h3>Welcome to your garden!</h3>
+        </div>
+
     );
 };
 
