c8ddb9b793a809c66a6eb2600381ebbd544c10d8
ikealmighty <isaacyates7@gmail.com>
2025-04-20 17:43:35 -0500

    bugfix: protected routes now redirect you to login when the token expires

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,8 +1,8 @@
-import { isLoggedIn } from "../utils/auth.js";
+import { isLoggedIn, isTokenExpired } from "../utils/auth.js";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  if (!isLoggedIn()) {
+  if (!isLoggedIn() || isTokenExpired()) {
     return <Navigate to="/login" replace />;
   }
 
